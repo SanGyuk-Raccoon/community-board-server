436ca8e87b0d4b1b6d4894facea13267fd2a86dc
ebs.kim <ebs.kim@samsung.com>
2023-12-17 14:41:35 +0900

    init

---
+++ community-board-server/src/main/java/com/raccoon/communityboardserver/CommunityBoardServerApplication.java
@@ -0,0 +1,13 @@
+package com.raccoon.communityboardserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunityBoardServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommunityBoardServerApplication.class, args);
+	}
+
+}
---
+++ community-board-server/src/test/java/com/raccoon/communityboardserver/CommunityBoardServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raccoon.communityboardserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityBoardServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
