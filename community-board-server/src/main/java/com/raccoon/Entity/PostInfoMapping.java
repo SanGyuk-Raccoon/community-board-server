c1bf9492f16c4c930ddaac0bacc562e1d3f1a4e4
ebs.kim <ebs.kim@samsung.com>
2023-12-18 23:14:28 +0900

    feature: add mapping interface

---
+++ community-board-server/src/main/java/com/raccoon/Entity/PostInfoMapping.java
@@ -0,0 +1,10 @@
+package com.raccoon.Entity;
+
+import java.util.Date;
+
+public interface PostInfoMapping {
+    Long getId();
+    String getTitle();
+    String getAuthor();
+    Date getDate();
+}
