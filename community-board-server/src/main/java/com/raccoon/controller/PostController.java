fbda0848eecca3a1809c16ac7934f687276d6f6f
ebs.kim <ebs.kim@samsung.com>
2023-12-18 22:49:59 +0900

    fix: CORS

--- community-board-server/src/main/java/com/raccoon/controller/PostController.java
+++ community-board-server/src/main/java/com/raccoon/controller/PostController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
+@CrossOrigin(origins = "*")
 @RestController
 public class PostController {
 
